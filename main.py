98453077880a85b33e5db63c549966dc9c502f88
Rafi <mr.k779@outlook.com>
2024-11-17 20:00:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ def scan_port(ip, port):
         result = s.connect_ex((ip, port))
         if result == 0:
             print(f"IP: {ip} | Port {port} is open")
-            # Optional: Infer service based on port number
-            common_ports = {22: "SSH", 80: "HTTP", 443: "HTTPS", 21: "FTP", 25: "SMTP"}
-            service = common_ports.get(port, "Unknown Service")
-            print(f"Service inferred: {service}")
+            return port
+
+    return None
 
 
 def scan_ip_range(ip_range, port_range):
-    """Scans a range of IPs and ports."""
-    with ThreadPoolExecutor(max_workers=100) as executor:
+    """Scans a range of IPs and ports and returns array of open ports."""
+    open_ports = []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
         for ip in ip_range:
             for port in port_range:
-                executor.submit(scan_port, ip, port)
+                future = executor.submit(scan_port, ip, port)
+                futures.append(future)
 
+        for future in futures:
+            result = future.result()
+            if result:
+                open_ports.append(result)
 
-port_range = range(20, 1025)  # Scan ports 20 through 1024
-
-# Start scanning
-ip_range = ["164.90.142.90"]
-scan_ip_range(ip_range, port_range)
+    return open_ports
