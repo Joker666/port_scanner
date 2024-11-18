56f339975735a6f5b5fd477eb53ca36d1f3e6ead
Rafi <mr.k779@outlook.com>
2024-11-17 21:56:30 -0400

    TCP route

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from robyn import Robyn
+from robyn import Request, Robyn
+
+import ports
 
 app = Robyn(__file__)
 
 
-@app.post("/api/scan/tcp")
-def index():
-    return "Hello World!"
+@app.get("/api/scan/tcp")
+def tcp_scan(request: Request):
+    ip_range = request.query_params.get("ips").split(",")
+
+    # Convert port range string to array of integers
+    port_input = request.query_params.get("ports")
+    if "-" in port_input:
+        start, end = map(int, port_input.split("-"))
+        port_range = list(range(start, end + 1))
+    else:
+        port_range = [int(port_input)]
+
+    open_ports_by_ip = ports.scan_ip_range(ip_range, port_range)
+    return open_ports_by_ip
 
 
 if __name__ == "__main__":
