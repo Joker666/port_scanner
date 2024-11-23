42a497f8bba57f7d470d70d4cf066d300ee00ff8
Rafi <mr.k779@outlook.com>
2024-11-23 18:53:06 -0400

    Update ports.py

--- ports.py
+++ ports.py
@@ -1,8 +1,11 @@
 import socket
 from concurrent.futures import ThreadPoolExecutor
 
+from robyn import logger
 from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import send, sr1
+
+from udp import analyze_response, create_udp_probe, get_common_udp_ports
 
 
 def scan_port(tracing_id, ip, port, protocol="tcp"):
@@ -35,12 +38,22 @@ def scan_port(tracing_id, ip, port, protocol="tcp"):
         else:
             return port, "filtered"
     else:  # UDP scan
+        probe_data = create_udp_probe(port)
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
                 s.settimeout(1)
-                s.sendto(b"", (ip, port))
+                s.sendto(probe_data, (ip, port))
                 data, _ = s.recvfrom(1024)
-                return port, "open"
+
+                is_valid, service_info = analyze_response(port, data)
+                if is_valid:
+                    return port, "open"
+
+                service_name = get_common_udp_ports().get(port, (f"port {port}", None))[
+                    0
+                ]
+                print(f"{service_name}: Unexpected response")
+                return port, "open|filtered"
         except socket.timeout:
             return port, "filtered"  # UDP ports often show as filtered
         except ConnectionRefusedError:
@@ -71,15 +84,24 @@ def scan_ip_range(tracing_id, ip_range, port_range, protocol="tcp", concurrency=
             port, status = future.result()
             if protocol == "tcp" and status == "filtered":
                 continue
-            if (protocol == "udp" or protocol == "syn") and (status == "filtered" or status == "closed"):
+            if (protocol == "udp" or protocol == "syn") and (
+                status == "filtered" or status == "closed"
+            ):
                 continue
 
             # Get service name for the port
-            service_name = get_service_name(port, protocol)
+            service_name = get_service_name(
+                port, "tcp" if protocol == "syn" else protocol
+            )
+
+            logger.info(
+                f"Scanned {ip}:{port} => Status: {status} | Service: {service_name}"
+            )
+
             # Store both service name and status
             open_ports_by_ip[ip][str(port)] = {
                 "service": service_name,
-                "status": status
+                "status": status,
             }
 
     return open_ports_by_ip
